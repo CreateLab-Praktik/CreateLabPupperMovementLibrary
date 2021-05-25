d74ed7f921e94f0a90326da573991905a2b75c98
MekaNekodemo <MartinTeitBuchwald@gmail.com>
2021-05-25 15:05:39 +0100

    New Class injectionInterface

--- PupperAutomation/ActionLoop.py
+++ PupperAutomation/ActionLoop.py
@@ -4,12 +4,14 @@ from multiprocessing import connection
 
 
 class ActionLoop:
-    def __init__(self,  PipeConnection: connection.Connection):
+    def __init__(self,  RobotConnection: connection.Connection, InjectionConnection: connection.Connection):
         """
             ActionLoop needs one end of a connection pipe tied to a Multiprocessing object.
         """
 
-        self.pipe = PipeConnection
+        self.robot_Connection = RobotConnection
+
+        self.injecter_Connection = InjectionConnection
 
         self.actionQueue = Queue()
 
@@ -21,24 +23,55 @@ class ActionLoop:
 
         while True:
 
+            # ## Check for injections
+            # if self.injecter_Connection.poll() == True:
+            #     message: ActionMessage = self.injecter_Connection.recv()
+
+            #     if message.interrupt == True:
+            #         self._handleInterruption(message)
+            #     else:
+            #         self._handleInjection(message)
+
+            ## check queue
             if self.actionQueue.qsize() != 0:
 
+                ## prepare message duration and sending
                 currentActionMsg: ActionMessage = self.actionQueue.get()
                 ticks = currentActionMsg.ticks
                 messageDone = False
 
                 while messageDone == False:
 
-                    self.pipe.send(currentActionMsg.toDictionary())
+                    # ## Check for interrupting messages
+                    # if self.injecter_Connection.poll() == True:
+                    #     message: ActionMessage = self.injecter_Connection.recv()
+
+                    #     if message.interrupt == True:
+                    #         ## if message is an interrupt kill current queue, and build new with interruption message
+                    #         self._handleInterruption(message)
+                    #         ticks == 0
+                    #         messageDone = True
+                    #         break
+                    #     else:
+                    #         self._handleInjection(message)
+
+                    self.robot_Connection.send(currentActionMsg.toDictionary())
 
                     ticks -= 1
 
                     if ticks <= 0:
                         messageDone = True
 
-            ## TODO test if sending a neutral message still is necessery
-            self.pipe.send(ActionMessage().toDictionary())
+            self.robot_Connection.send(ActionMessage().toDictionary())
 
 
 
+    def _handleInterruption(self, message: ActionMessage ):
+        print("Intterrupt read, interrupting...")
+        self.actionQueue = Queue()
+        self.actionQueue.put(message)
 
+        
+    def _handleInjection(self, message: ActionMessage):
+        print("Injection read and message queued")
+        self.actionQueue.put(message)
